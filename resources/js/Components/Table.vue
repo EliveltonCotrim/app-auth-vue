2345b9a12b1c26d7db6e7554abed3bbaf0aace07
Elivelton <elivelton.gbi@gmail.com>
2024-02-08 12:23:11 -0300

    configuraçao do breeze

---
+++ resources/js/Components/Table.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+<template>
+  <table class="min-w-full bg-white border border-gray-300">
+    <thead>
+      <tr>
+        <th class="py-2 px-4 border-b">Nome</th>
+        <th class="py-2 px-4 border-b">Idade</th>
+        <th class="py-2 px-4 border-b">Cidade</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="py-2 px-4 border-b">João</td>
+        <td class="py-2 px-4 border-b">25</td>
+        <td class="py-2 px-4 border-b">São Paulo</td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border-b">Maria</td>
+        <td class="py-2 px-4 border-b">30</td>
+        <td class="py-2 px-4 border-b">Rio de Janeiro</td>
+      </tr>
+      <!-- Adicione mais linhas conforme necessário -->
+    </tbody>
+  </table>
+</template>
